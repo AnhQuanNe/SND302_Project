a74d2a0286793aa09e0f3d20224d47c1b859163b
AnhQuanNe <quannguyen.dn2005@gmail.com>
2026-06-16 11:22:15 +0700

    update admin.service.js và auth.service.js

--- frontend/src/services/auth.service.js
+++ frontend/src/services/auth.service.js
@@ -1,10 +1,19 @@
-import axios from "axios";
+// import axios from "axios";
 
-const API =
-  "http://localhost:5000/api/auth";
+// const API =
+//   "http://localhost:5000/api/auth";
+
+// export const login = (data) =>
+//   axios.post(`${API}/login`, data);
+
+// export const register = (data) =>
+//   axios.post(`${API}/register`, data);
+import API from "./api";
+
+const BASE = "/auth";
 
 export const login = (data) =>
-  axios.post(`${API}/login`, data);
+  API.post(`${BASE}/login`, data);
 
 export const register = (data) =>
-  axios.post(`${API}/register`, data);
+  API.post(`${BASE}/register`, data);
